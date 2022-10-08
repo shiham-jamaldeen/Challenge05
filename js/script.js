ebc57d82f1ca29521972fdc4db57efaf718c3413
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2022-10-08 13:15:22 +1030

    added bug fixed local storage save feature

--- js/script.js
+++ js/script.js
@@ -1,48 +1,59 @@
-//get current date and time
+//gloabl variables
 var today = moment();
 var currentHour = moment().hours();
 var currentMin = moment().minutes();
 
-//display current date and time
-$("#currentDay").text(today.format("dddd, MMMM, Do YYYY"));
-$("#currentHH").text(today.format("HH : MM"));
+function displayCurrentDateAndTime() {
+  //display current date and time
+  $("#currentDay").text(today.format("dddd, MMMM, Do YYYY"));
+  $("#currentHH").text(today.format("HH : MM"));
+  console.log(currentHour + " : " + currentMin);
+}
 
-console.log(currentHour + " : " + currentMin);
+function displayRowColour() {
+  //loop through the row
+  $(".row").each(function (index, element) {
+    //check the "div" element (child)
+    let stringInt = parseInt(element.children[0].textContent); //convert from string to integer
+    //get the current hour
+    let currentHours = parseInt(currentHour); //convert from string to integer
+    // console.log("element coverted: " + stringInt);
+    //console.log("current hour: " + currentHours);
+    // console.log(element);
+    //console.log(element.children[0].textContent);
 
-//this code removes the placeholder for all items!
+    //condition to check the current hour and display the colour, in the child "textarea"
+    if (stringInt < currentHours) {
+      $(element.children[1]).addClass("past");
+    } else if (stringInt > currentHours) {
+      $(element.children[1]).addClass("future");
+    } else {
+      $(element.children[1]).addClass("present");
+    }
+  });
+}
 
-//loop through the row
-$(".row").each(function (index, element) {
-  //check the "div" element (child)
-  let stringInt = parseInt(element.children[0].textContent); //convert from string to integer
-  //get the current hour
-  let currentHours = parseInt(currentHour); //convert from string to integer
-  // console.log("element coverted: " + stringInt);
-  //console.log("current hour: " + currentHours);
-  // console.log(element);
-  //console.log(element.children[0].textContent);
-
-  //condition to check the current hour and display the colour, in the child "textarea"
-  if (stringInt < currentHours) {
-    $(element.children[1]).addClass("past");
-  } else if (stringInt > currentHours) {
-    $(element.children[1]).addClass("future");
-  } else {
-    $(element.children[1]).addClass("present");
-  }
-});
-//disable text and button for past time **IMPROVEMENT**
-
-//check keypress
-
-$(".row").click(function (event) {
-  //window.alert("I was clicked");
-  console.log(event.target.id);
-  let buttonPressedID = parseInt(event.target.id);
-  if (buttonPressedID == 9) {
-    //read the value from the
-    let contentsText = $("$9-1").val();
-    console.log(contentsText);
-    localStorage.setItem("key-9-1", contentsText);
-  }
-});
+function saveContent() {
+  $(".row").click(function (event) {
+    console.log(event.target.id);
+    //get id of button pressed
+    let buttonPressedID = parseInt(event.target.id);
+    //get element id of textarea
+    var textAreaId = "#" + buttonPressedID + "-1";
+    //get element id of textarea
+    let contentsText = $(textAreaId).val();
+    //store value of textarea
+    localStorage.setItem(buttonPressedID, contentsText);
+    console.log("button pressed: " + buttonPressedID);
+    console.log("context: " + contentsText);
+  });
+}
+function getContent() {
+  //select the row
+  //
+}
+//main program
+displayCurrentDateAndTime();
+displayRowColour();
+saveContent();
+getContent();
